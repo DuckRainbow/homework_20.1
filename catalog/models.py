58b2f8c8b2cd9a6cd26753bc9ca3e696788179c1
DuckRainbow <sillestyna@gmail.com>
2024-05-06 17:33:49 +0500

    1-3

---
+++ catalog/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+
+
+class Category(models.Model):
+    title = models.CharField(
+        max_length=50, verbose_name="Наименование", blank=True, null=True
+    )
+    description = models.TextField(verbose_name="Описание", blank=True, null=True)
+
+    def __str__(self):
+        # Строковое отображение объекта
+        return f"{self.title}"
+
+    class Meta:
+        verbose_name = "категория"
+        verbose_name_plural = "категории"
+
+
+# Create your models here.
+class Product(models.Model):
+    title = models.CharField(
+        max_length=50, verbose_name="Наименование", blank=True, null=True
+    )
+    description = models.TextField(verbose_name="Описание", blank=True, null=True)
+    image = models.ImageField(
+        upload_to="catalog/image", blank=True, null=True, verbose_name="Изображение"
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        verbose_name="Категория",
+        null=True,
+        blank=True,
+        related_name="products",
+    )
+    price = models.IntegerField(verbose_name="Цена за покупку", blank=True, null=True)
+    created_at = models.DateTimeField(
+        verbose_name="Дата создания", blank=True, null=True
+    )
+    updated_at = models.DateTimeField(
+        verbose_name="Дата последнего изменения", blank=True, null=True
+    )
+
+    def __str__(self):
+        # Строковое отображение объекта
+        return (
+            f"Продукт {self.title} из категории {self.category}, стоимость {self.price}"
+        )
+
+    class Meta:
+        verbose_name = "продукт"
+        verbose_name_plural = "продукты"
