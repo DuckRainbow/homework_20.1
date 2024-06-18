fe3d18b845606ca5319a63a463d654af1d1b872e
DuckRainbow <sillestyna@gmail.com>
2024-06-18 16:59:57 +0500

    all done

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,9 +1,20 @@
 from django import forms
+from django.forms import BooleanField
 
-from catalog.models import Product
+from catalog.models import Product, Version
 
 
-class ProductForm(forms.ModelForm):
+class FormStyleMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                field.widget.attrs['class'] = 'form-control'
+
+
+class ProductForm(FormStyleMixin, forms.ModelForm):
     class Meta:
         model = Product
         fields = ('title', 'description', 'image', 'category', 'price')
@@ -16,3 +27,9 @@ class ProductForm(forms.ModelForm):
             raise forms.ValidationError('Ошибка, связанная с названием продукта')
 
         return cleaned_data
+
+
+class VersionForm(FormStyleMixin, forms.ModelForm):
+    class Meta:
+        model = Version
+        fields = ('title', 'counter', 'currented')
