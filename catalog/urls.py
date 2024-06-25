39fb50f1d66f0e1275f847c4e52a0fa18cbc41e6
DuckRainbow <sillestyna@gmail.com>
2024-06-25 21:48:35 +0500

    сache

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from catalog.apps import CatalogConfig
 
@@ -9,7 +10,7 @@ app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='products_list'),
-    path('catalog/<int:pk>/', ProductDetailView.as_view(), name='products_detail'),
+    path('catalog/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='products_detail'),
     path('catalog/create', ProductCreateView.as_view(), name='products_create'),
     path('catalog/<int:pk>/update/', ProductUpdateView.as_view(), name='products_update'),
     path('catalog/<int:pk>/delete/', ProductDeleteView.as_view(), name='products_delete'),
